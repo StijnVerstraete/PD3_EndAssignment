921550025a8c2229bf0f2003c5aefd74bb89f7cb
StijnVerstraete <43540802+StijnVerstraete@users.noreply.github.com>
2019-01-14 10:38:55 +0100

    Fix box push

--- Assets/Scripts/PushableBlock.cs
+++ Assets/Scripts/PushableBlock.cs
@@ -33,6 +33,8 @@ public class PushableBlock : MonoBehaviour {
                     _player.GetComponent<CharacterControllerBehaviour>().IsPushing = true;
 
                      GetComponent<Rigidbody>().isKinematic = false;
+                    //disable collider
+                    _player.GetComponent<CapsuleCollider>().enabled = false;
                 }
                //set timer to avoid multiple button presses in short succession
                 _delayTimer = 60;
@@ -46,6 +48,9 @@ public class PushableBlock : MonoBehaviour {
             _player.GetComponent<CharacterControllerBehaviour>().IsPushing = false;
 
             GetComponent<Rigidbody>().isKinematic = true;
+
+            //enable collider
+            _player.GetComponent<CapsuleCollider>().enabled = false;
         }
     }
     private void CalculateSnappingPoint(Vector3 forwardWall, Vector3 col)
@@ -56,14 +61,12 @@ public class PushableBlock : MonoBehaviour {
         #endregion offsetvalues
         if (forwardWall.x != 0)
         {
-            //- (2 * forwardWall.x)
             _player.transform.position = new Vector3(col.x + (xzOffset * -transform.forward.x), _player.transform.position.y, _player.transform.position.z);
 
             Debug.Log("X");
         }
         else if (forwardWall.z != 0)
         {
-            //- (2 * forwardWall.z
             _player.transform.position = new Vector3(_player.transform.position.x, _player.transform.position.y, col.z + (xzOffset * -transform.forward.z));
 
             Debug.Log("Z");
@@ -72,15 +75,13 @@ public class PushableBlock : MonoBehaviour {
         Vector3 newRot = new Vector3(_player.transform.eulerAngles.x, transform.eulerAngles.y, _player.transform.eulerAngles.z);
         _player.transform.rotation = Quaternion.Euler(newRot);
         Debug.Log("SnappingPointCalculated");
-
     }
     private void ApplyForce()
     {
+        float forceToApply = 12;
         if (Input.GetAxis("Vertical") > 0)
         {
-            int forceToApply = 2;
-            Vector3 force = new Vector3(transform.forward.x * forceToApply, 0,transform.forward.z * forceToApply);
-            GetComponent<Rigidbody>().AddForce(force);
+            GetComponent<Rigidbody>().AddForce(_player.transform.forward * forceToApply,ForceMode.Force);
         }
     }
 
